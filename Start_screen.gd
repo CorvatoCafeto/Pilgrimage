a994fb75856631d4f0b8d1e836fb8c376fe09da8
CorvatoCafeto <emma.fs92@gmail.com>
2019-02-07 13:00:00 -0300

    Little fix in the menu screen

--- Start_screen.gd
+++ Start_screen.gd
@@ -4,6 +4,8 @@ extends Node2D
 # var a = 2
 # var b = "text"
 
+var pressed = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimationPlayer.current_animation = "stop"
@@ -11,10 +13,12 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_just_pressed("blink") or Input.is_action_just_pressed("fire"):
-		$Prompt.hide()
-		$Timer.start()
-		$AnimationPlayer.play("Intro")
+	if pressed == false:
+		if Input.is_action_pressed("blink") or Input.is_action_pressed("fire"):
+			$Prompt.hide()
+			$AnimationPlayer.play("Intro")
+			pressed = true
 
-func _on_Timer_timeout():
-		get_tree().change_scene("res://Main.tscn")
+func _on_AnimationPlayer_animation_finished(anim_name):
+	get_tree().change_scene("res://Main.tscn")
+
