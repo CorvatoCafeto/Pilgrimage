29fd19e7fa37c0edf56842d22f01ee0b3484f2c8
Korvato <emma.fs92@gmail.com>
2023-12-19 18:53:18 -0300

    Sorted files

---
+++ interface/Start_screen.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+var pressed = false
+
+func _ready():
+#	$AnimationPlayer.current_animation = "stop"
+	pass
+
+func _process(_delta):
+	if pressed == false:
+		if Input.is_action_pressed("blink") or Input.is_action_pressed("fire"):
+			$Prompt.hide()
+			$AnimationPlayer.play("Intro")
+			pressed = true
+			
+func _on_AnimationPlayer_animation_finished(_anim_name):
+	if get_tree().change_scene("res://levels/Main.tscn") != OK:
+		print ("An unexpected error occured when trying to switch to the Main scene")
