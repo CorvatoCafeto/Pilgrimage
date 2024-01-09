e7b341bf15db8f241ede1c8961d27ec23c365a3e
Korvato <emma.fs92@gmail.com>
2024-01-08 23:58:54 -0300

    Character controller refactor

--- witch/witch.gd
+++ witch/witch.gd
@@ -12,17 +12,57 @@ const BULLET_VELOCITY = 1
 const SHOOT_TIME_SHOW_WEAPON = 0.2
 const FIREBALL = preload("res://fireball/Fireball.tscn")
 const DASH_TIME = 1
+const GRAVITY = 900
+const LEFT = -1
+const RIGHT = 1
 
-var GRAVITY_VEC = Vector2(0, 900)
-var linear_vel = Vector2()
+var GRAVITY_VEC = Vector2(0, GRAVITY)
+var BLINK_GRAVITY = 100  # Adjust this value as needed
+var character_velocity = Vector2()
 var onair_time = 0 #
 var on_floor = false
 var has_double_jumped = false
-var shoot_time=99999 #time since last shot
-var anim=""
 var is_cat = false
 var dashing = false
 var dash_acc = 0
+
+func shoot():
+	var fireball = FIREBALL.instance()
+	fireball.set_fireball_direction(sign($Position2D.position.x))
+	get_parent().add_child(fireball)
+	fireball.global_position = $Position2D.global_position #use node for shoot position
+	$sound_shoot.play()
+
+func blink():
+	character_velocity.x = $Sprite.flip_h ? -500 : 500
+	dashing = true
+	GRAVITY_VEC = Vector2(0, BLINK_GRAVITY)
+	$sound_blink.play()
+
+	if dashing:
+		dash_acc += delta
+	#if we dashed for longer than our DASH_TIME we stop and reset our accumulator
+	if dash_acc >= DASH_TIME:    
+		dashing = false
+		character_velocity = Vector2(0, 0)
+		dash_acc = 0
+		GRAVITY_VEC = Vector2(0, GRAVITY)
+
+func transform_to_cat(transform):
+	is_cat = transform
+	$CollisionShape2D.scale.y = transform ? 0.5 : 1
+	$CollisionShape2D.scale.x = transform ? 0.5 : 1
+	$sound_cat.play()
+
+func flip_sprite_and_adjust_position(direction):
+	$Sprite.flip_h = direction == RIGHT
+	if sign($Position2D.position.x) != direction:
+		$Position2D.position.x *= -1
+
+func detect_floor():
+	if is_on_floor():
+		onair_time = 0
+		has_double_jumped = false
 
 func _physics_process(delta):
 	#increment counters
@@ -31,12 +71,10 @@ func _physics_process(delta):
 	shoot_time += delta
 
 	### MOVEMENT ###
+	character_velocity += delta * GRAVITY_VEC
+	character_velocity = move_and_slide(character_velocity, FLOOR_NORMAL, SLOPE_SLIDE_STOP)
+	detect_floor()
 
-	# Apply Gravity
-	linear_vel += delta * GRAVITY_VEC
-	# Move and Slide
-	linear_vel = move_and_slide(linear_vel, FLOOR_NORMAL, SLOPE_SLIDE_STOP)
-	# Detect Floor
 	if is_on_floor():
 		onair_time = 0
 		has_double_jumped = false
@@ -47,93 +85,57 @@ func _physics_process(delta):
 	### CONTROL ###
 
 	# Horizontal Movement
-	var target_speed = 0
+	var horizontal_movement = 0
 	if Input.is_action_pressed("left"):
-		target_speed += -1
-		$Sprite.flip_h = true
-		if sign($Position2D.position.x) == 1:
-			$Position2D.position.x *= -1
+		horizontal_movement += LEFT
+		flip_sprite_and_adjust_position(LEFT)
 	if Input.is_action_pressed("right"):
-		target_speed +=  1
-		$Sprite.flip_h = false
-		if sign($Position2D.position.x) == -1:
-			$Position2D.position.x *= -1
+		horizontal_movement += RIGHT
+		flip_sprite_and_adjust_position(RIGHT)
 		
 		
-	target_speed *= WALK_SPEED
-	linear_vel.x = lerp(linear_vel.x, target_speed, 0.1)
+	horizontal_movement *= WALK_SPEED
+	character_velocity.x = lerp(character_velocity.x, horizontal_movement, 0.1)
 
 	# Jumping
+	if Input.is_action_just_pressed("up") and (is_on_floor() or (Upgrades.BROOM and !has_double_jumped)):
+		character_velocity.y = -JUMP_SPEED
+		$sound_jump.play()
+		has_double_jumped = !is_on_floor()
+
 	if on_floor and Input.is_action_just_pressed("up"):
-		linear_vel.y = -JUMP_SPEED
+		character_velocity.y = -JUMP_SPEED
 		$sound_jump.play()
 	elif Upgrades.BROOM and !has_double_jumped and Input.is_action_just_pressed("up"):
-		linear_vel.y = -JUMP_SPEED
+		character_velocity.y = -JUMP_SPEED
 		$sound_jump.play()
 		has_double_jumped = true
 		
 	# Transforming
-	if Upgrades.CAT:
-		if !is_cat and Input.is_action_just_pressed("down"):
-			is_cat = true
-			$CollisionShape2D.scale.y = 0.5
-			$CollisionShape2D.scale.x = 0.5
-			$sound_cat.play()
-		elif is_cat and Input.is_action_just_pressed("down"):
-			$CollisionShape2D.scale.y = 1
-			$CollisionShape2D.scale.x = 1
-			is_cat = false
-			$sound_cat.play()
+	if Upgrades.CAT and Input.is_action_just_pressed("down"):
+		transform_to_cat(!is_cat)
 		
 	# Blinking
-	if Upgrades.TUNIC:
-		if Input.is_action_just_pressed("blink"):
-			if !$Sprite.flip_h:
-				linear_vel = Vector2(500, 0)
-				dashing = true
-				#GRAVITY_VEC = Vector2(0, 450)
-				$sound_blink.play()
-			elif $Sprite.flip_h:
-				linear_vel = Vector2(-500, 0)
-				dashing = true
-				#GRAVITY_VEC = Vector2(0, 450)
-				$sound_blink.play()
-		
-			if dashing:
-				dash_acc += delta
-			#if we dashed for longer than our DASH_TIME we stop and reset our accumulator
-			if dash_acc >= DASH_TIME:    
-				dashing = false
-				linear_vel = Vector2(0, 0)
-				dash_acc = 0
-				GRAVITY_VEC = Vector2(0, 900)
+	if Upgrades.TUNIC and Input.is_action_just_pressed("blink"):
+		blink()
 			
 	# Shooting
-	if Upgrades.WAND:
-		if !is_cat and Input.is_action_just_pressed("fire"):
-			
-			var fireball = FIREBALL.instance()
-			if sign($Position2D.position.x) == 1:
-				fireball.set_fireball_direction(1)
-			else:
-				fireball.set_fireball_direction(-1)	
-			get_parent().add_child(fireball)
-			fireball.global_position = $Position2D.global_position #use node for shoot position
-			$sound_shoot.play()
-		
+	if Upgrades.WAND and !is_cat and Input.is_action_just_pressed("fire"):
+		shoot()
+
 	### ANIMATION ###
 	var new_anim = "Idle"
 	if is_cat:
 		new_anim = "Idle_cat"
 
 	if on_floor:
-		if linear_vel.x < -SIDING_CHANGE_SPEED:
+		if character_velocity.x < -SIDING_CHANGE_SPEED:
 			#witch_sprite.scale.x = -1
 			new_anim = "Walking"
 			if is_cat:
 				new_anim = "Running_cat"
 
-		if linear_vel.x > SIDING_CHANGE_SPEED:
+		if character_velocity.x > SIDING_CHANGE_SPEED:
 			#witch_sprite.scale.x = 1
 			new_anim = "Walking"
 			if is_cat:
@@ -149,7 +151,7 @@ func _physics_process(delta):
 		#	witch_sprite.scale.x = 1
 		#	$witch_sprite.flip_h = false
 
-		if linear_vel.y < 0:
+		if character_velocity.y < 0:
 			new_anim = "Jumping"
 			if is_cat:
 				new_anim = "Jumping_cat"
